d1d8cd3c5e79de695d59fc9dda35ed44731c28a5
Jeyson Triana <stiventriana0324@gmail.com>
2025-03-06 17:56:11 -0500

    Se agregan los CORS al back

---
+++ calendario-back/config/cors.php
@@ -0,0 +1,5 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+?>
