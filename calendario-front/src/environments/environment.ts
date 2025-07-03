9cbdd54399d02cdf94b90ef510491957858e84f0
Jeyson Triana <stiventriana0324@gmail.com>
2025-07-03 17:35:44 -0500

    Actualizar URLs en el archivo de entorno para apuntar a la producción

--- calendario-front/src/environments/environment.ts
+++ calendario-front/src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: true,
     // URL base del backend de la aplicación
-    apiUrl: 'http://10.0.26.31:8086/',
+    apiUrl: 'https://testcalendarios.uniminuto.edu/api/',
   
     // URL de redirección del frontend (app Angular)
-    baseUrl: 'http://10.0.26.31:8085',
+    baseUrl: 'https://testcalendarios.uniminuto.edu/login',
 
     // Configuración de MSAL para autenticación con Azure AD
     clientId: 'd72569ba-95a4-4dd7-b0d2-f9d6d4d6a223',
     authority: 'https://login.microsoftonline.com/b1ba85eb-a253-4467-9ee8-d4f8ed4df300',
-    redirectUri: 'http://10.0.26.31:8085/dashboard'
+    redirectUri: 'https://testcalendarios.uniminuto.edu/dashboard'
 };
